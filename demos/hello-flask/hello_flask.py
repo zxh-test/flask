322e84025f244573ab8b4027f54c04c77f9c9d0e
zxh-test <dhaoren23@126.com>
2021-11-10 16:05:16 +0800

    delete .env

--- demos/hello-flask/hello_flask.py
+++ demos/hello-flask/hello_flask.py
@@ -3,5 +3,25 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_flask():
-    return "<h1>hello flask</h1>"
+def index():
+    return "<h1>Hello Flask</h1>"
+
+
+# 多个URL对应一个视图函数
+@app.route('/hi')
+@app.route('/hello')
+def say_hello():
+    return '<h1>Hello, Flask</h1>'
+
+
+# 动态路由，URL可以有默认值
+@app.route('/greet/', defaults={'name': 'default name'})
+@app.route('/greet/<name>')
+def greet(name):
+    return '<h1>Hello, %s</h1>' % name
+
+
+import click 
+@app.cli.command()
+def hello():
+   click.echo("hello, cli")

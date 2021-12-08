9a52c7d782ce67711bfe115b03366c28815a3f0a
zxh-test <dhaoren23@126.com>
2021-12-08 17:12:19 +0800

    change something

--- demos/form/app.py
+++ demos/form/app.py
@@ -1,4 +1,4 @@
-from flask import render_template, Flask,request
+from flask import render_template, Flask, flash, redirect, url_for, request
 from form import LoginForm
 
 app = Flask(__name__)
@@ -6,9 +6,16 @@ app = Flask(__name__)
 app.secret_key = 'secret string'
 
 
+@app.route('/')
+def index():
+    return 'Welcome {}'
+
+
 @app.route('/basic', methods=['GET', 'POST'])
 def basic():
     form = LoginForm()
     if form.validate_on_submit():
-        return 'welcome {}'.format(request.form.get('username'))
+        username = form.username.data
+        flash('Welcome %s' % username)
+        return redirect(url_for('basic'))
     return render_template('basic.html', form=form)
--- demos/form/form.py
+++ demos/form/form.py
@@ -1,11 +1,12 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField, EmailField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length
 
 
 class LoginForm(FlaskForm):
-    username = StringField(label='User name', validators=[DataRequired()], render_kw={'placeholder': 'your name'})
-    password = PasswordField(label='Password', validators=[Length(6, 16, message='密码格式错误')],
+    username = StringField(label='Username', validators=[DataRequired(message='please input username')],
+                           render_kw={'placeholder': 'your name'})
+    password = PasswordField(label='Password', validators=[Length(6, 16, message='Password length is wrong')],
                              render_kw={'placeholder': 'your password'})
     remember = BooleanField(label='Remember')
     submit = SubmitField(label='Submit')
